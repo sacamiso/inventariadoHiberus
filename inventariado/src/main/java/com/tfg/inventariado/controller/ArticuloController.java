6585cd8f505876814b8dbe17d26c2b59dc7ca488
Sara <scamison@hiberus.com>
2024-02-23 14:00:38 +0100

    Solucion a cros

--- inventariado/src/main/java/com/tfg/inventariado/controller/ArticuloController.java
+++ inventariado/src/main/java/com/tfg/inventariado/controller/ArticuloController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.tfg.inventariado.provider.ArticuloProvider;
 
 @RestController
 @RequestMapping("/articulo")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ArticuloController {
 	
 	@Autowired
--- inventariado/src/main/java/com/tfg/inventariado/controller/RolController.java
+++ inventariado/src/main/java/com/tfg/inventariado/controller/RolController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.tfg.inventariado.dto.RolDto;
 import com.tfg.inventariado.provider.RolProvider;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/rol")
 public class RolController {
 
